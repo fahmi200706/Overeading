f06b830dd744011be4a0f11057081ba97d76d2c3
fahmi200706 <fahmi.asyahdan@gmail.com>
2025-03-04 11:29:28 +0700

    Add files via upload
    Commit overeading

---
+++ A.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OverReading;
+
+/**
+ *
+ * @author DELL
+ */
+public class A {
+    public void tamplikanKeLayar (){
+        System.out.println("Method milik kelas A di panggil");
+    } 
+}
